ec04c33695f71ef35d113b21d6ee202fedd82cc1
dkiriinya <dkiriinya06sc@gmail.com>
2023-12-07 22:18:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr,eventName){
+    let newArray = []
+    for(let i = 0; i<arr.length; i++){    
+        newArray[i] = (`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArray
+
+}
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num)
+        num --
+    }    
+}
+
